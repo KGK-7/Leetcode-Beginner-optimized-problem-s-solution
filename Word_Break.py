b29f16e961c9e7ef5d1dff057451cf55dc51e76a
KGK-7 <tamilangokul58@gmail.com>
2025-06-30 22:20:02 +0530

    updated solution

---
+++ Word_Break.py
@@ -0,0 +1,20 @@
+import collections
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordSet=set(wordDict)
+        memo={}
+        
+        def canBreak(start):
+            if start == len(s):
+                return True
+            if start in memo:
+                return memo[start]
+
+            for end in range(start+1, len(s)+1):
+                if s[start:end] in wordSet and canBreak(end):
+                    memo[start]= True
+                    return True
+            memo[start]=False
+            return False
+        return canBreak(0)
