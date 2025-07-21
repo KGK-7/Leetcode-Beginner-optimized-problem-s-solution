37866cad57a8860ab5a28f48556e8c5b9b9a3d97
KGK-7 <tamilangokul58@gmail.com>
2025-07-21 14:24:34 +0530

    feat: python solution for returning count value by matching item with a rule

---
+++ count items matching a rule.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        keys= {"type": 0, "color": 1, "name" : 2 }
+        index = keys[ruleKey]
+        count = 0
+
+        for i in items:
+            if i[index] == ruleValue:
+                count +=1
+        return count
+
+s= Solution()
+print(s.countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], "color", "silver"))
+print(s.countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], "type", "phone"))
+print(s.countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], "name", "pixel"))
+print(s.countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], "color", "blue"))
+print(s.countMatches([["phone","blue","pixel"],["computer","silver","lenovo "],["phone","gold","iphone"]], "type", "computer")) 
