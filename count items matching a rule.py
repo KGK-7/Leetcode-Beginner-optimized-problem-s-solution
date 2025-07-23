3c3e11a5486c7b9c9af343c4ee4e88833853c559
KGK-7 <tamilangokul58@gmail.com>
2025-07-23 22:54:54 +0530

    fix: package included and updated solution python solution for count items matching based on given rule

--- count items matching a rule.py
+++ count items matching a rule.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
         keys= {"type": 0, "color": 1, "name" : 2 }
